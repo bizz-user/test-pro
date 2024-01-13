4ea791f432558ccdf2d05798e6cb0fcd5de77868
linhvq <linhvuquang8@gmail.com>
2024-01-14 03:14:51 +0700

    a

--- project/settings.py
+++ project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "/var/www/supernginx.lol/static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "myapp/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
